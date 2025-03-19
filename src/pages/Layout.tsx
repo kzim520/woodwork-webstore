f43b0bcd0be0a99e57ff8671ecc8d8afe43c6232
Kevin Zimmer <krzimmer@mines.edu>
2025-03-18 18:38:53 -0600

    Added Layout

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,25 @@
+const Layout = () => {
+  return (
+    <>
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <a className="nav-link active" href="#">
+            Shop
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Custom Orders
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            My Work
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Layout;
